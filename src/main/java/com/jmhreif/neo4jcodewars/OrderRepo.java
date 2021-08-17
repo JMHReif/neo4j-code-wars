4bf81059094296aa2a1c7f355d279380a5ec966c
Jennifer Reif <jennifer.reif@neo4j.com>
2021-08-17 15:00:53 -0500

    Update props and test endpoints

--- src/main/java/com/jmhreif/neo4jcodewars/OrderRepo.java
+++ src/main/java/com/jmhreif/neo4jcodewars/OrderRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 interface OrderRepo extends CrudRepository<Order, Long> {
     Order findOrderByOrderId(Long orderId);
 
-    @Query("MATCH (p:Product)<-[rel:INCLUDES]-(o:Order) WHERE p.productName =~ ('(?i).*'+{productName}+'.*') RETURN *")
+    @Query("MATCH (p:Product)<-[rel:INCLUDES]-(o:Order) WHERE p.productName =~ ('(?i).*'+$productName+'.*') RETURN *;")
     Iterable<Order> findOrdersByOrderedProductsContaining(String productName);
 }
--- src/test/java/com/jmhreif/neo4jcodewars/Neo4jCodeWarsApplicationTests.java
+++ src/test/java/com/jmhreif/neo4jcodewars/Neo4jCodeWarsApplicationTests.java
@@ -1,16 +1,13 @@
 package com.jmhreif.neo4jcodewars;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class Neo4jCodeWarsApplicationTests {
+class Neo4jCodeWarsApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
