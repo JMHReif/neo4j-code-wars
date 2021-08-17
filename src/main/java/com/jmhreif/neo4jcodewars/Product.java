68ed4343201b1b32642058bc1dabd73ec1d759cb
Jennifer Reif <jennifer.reif@neo4j.com>
2021-08-17 14:13:49 -0500

    Fix missed changes

--- src/main/java/com/jmhreif/neo4jcodewars/Product.java
+++ src/main/java/com/jmhreif/neo4jcodewars/Product.java
@@ -5,36 +5,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-<<<<<<< Updated upstream
-import org.neo4j.ogm.annotation.GeneratedValue;
-import org.neo4j.ogm.annotation.Id;
-import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Relationship;
-
-=======
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
 import java.util.ArrayList;
->>>>>>> Stashed changes
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
-<<<<<<< Updated upstream
-@NodeEntity
-public class Product {
-    @Id @GeneratedValue
-=======
 @Node
 class Product {
     @Id
     @GeneratedValue
->>>>>>> Stashed changes
     private Long neoId;
+
     @NonNull
     private Long productId;
 
@@ -42,13 +29,7 @@ class Product {
     private Integer unitsInStock, unitsOnOrder, reorderLevel, discontinued;
     private double unitPrice;
 
-<<<<<<< Updated upstream
-    @Relationship(type = "INCLUDES", direction = Relationship.INCOMING)
-    @JsonIgnoreProperties("product")
-    private List<OrderedProduct> productOrders;
-=======
     @Relationship(type = "INCLUDES", direction = Relationship.Direction.INCOMING)
     @JsonIgnoreProperties("product")
     private List<OrderedProduct> productOrders = new ArrayList<>();
->>>>>>> Stashed changes
 }
