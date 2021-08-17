0f9826c3d6d3e6eb39bc3651df389bc43e3fad97
Jennifer Reif <jennifer.reif@neo4j.com>
2021-08-17 15:03:18 -0500

    Fix to add test class

---
+++ src/test/java/com/jmhreif/neo4jcodewars/Neo4jCodeWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmhreif.neo4jcodewars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jCodeWarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
